98952f18631e4875f14d4e3b9b7d6b6974eb3162
lgottber <lgottber@gmail.com>
2026-03-19 12:18:49 -0400

    fixing linting issues

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Auth0Provider } from "@auth0/nextjs-auth0/client";
+import dynamic from "next/dynamic";
+
+const Auth0Provider = dynamic(
+  () => import("@auth0/nextjs-auth0/client").then((mod) => mod.Auth0Provider),
+  { ssr: false },
+);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <Auth0Provider>{children}</Auth0Provider>;
