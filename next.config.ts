0c99bd9eebe2ec83ee87d85b86b27f229d2aaa54
rtrigoso <renzotrigoso@gmail.com>
2026-04-02 18:48:43 -0400

    attempts to fix issue with path resolution by using the nextjs config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   transpilePackages: ["@auth0/auth0-react", "@auth0/auth0-spa-js"],
   turbopack: {
     root: ".",
