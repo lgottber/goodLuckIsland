9ee491f7050d54d8255b723eb986f407814f7fe5
rtrigoso <renzotrigoso@gmail.com>
2026-04-02 17:57:42 -0400

    turbopack static export uses package.json, which was not up to date since the project does not use it until now

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   transpilePackages: ["@auth0/auth0-react", "@auth0/auth0-spa-js"],
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
