a1dd853b2c5e46b28c896c1706cd5706e0b255f4
lgottber <lgottber@gmail.com>
2026-04-27 14:21:03 -0400

    privacy policy

--- src/app/HeroCtaGuest.jsx
+++ src/app/HeroCtaGuest.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HeroCtaGuest() {
   return (
     <>
-      <a href="/auth/login?screen_hint=signup" className="cta-primary">
+      <a href="/signup" className="cta-primary">
         Start Your Lifestyle Discovery
       </a>
       <Link href="/profile" className="cta-ghost">
