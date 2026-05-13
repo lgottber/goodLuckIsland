4b95113158f11195b2542b35484b7dccdb018917
rtrigoso <renzotrigoso@gmail.com>
2026-05-13 15:30:20 -0400

    the env extraction for authentication variables can be made to happen only once

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { SupabaseAuthSync } from "../components/SupabaseAuthSync";
 import { BlockedGuard } from "../components/BlockedGuard";
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from "../lib/auth0";
 
 // @auth0/auth0-spa-js contains browser-only module chunks that webpack cannot
 // load during Next.js prerendering. Loading with ssr: false prevents the
@@ -16,15 +17,13 @@ const Auth0Provider = dynamic(
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const auth0Domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
-  const auth0ClientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
-  if (!auth0Domain) throw new Error("NEXT_PUBLIC_AUTH0_DOMAIN is not set");
-  if (!auth0ClientId) throw new Error("NEXT_PUBLIC_AUTH0_CLIENT_ID is not set");
+  if (!AUTH0_DOMAIN) throw new Error("NEXT_PUBLIC_AUTH0_DOMAIN is not set");
+  if (!AUTH0_CLIENT_ID) throw new Error("NEXT_PUBLIC_AUTH0_CLIENT_ID is not set");
 
   return (
     <Auth0Provider
-      domain={auth0Domain}
-      clientId={auth0ClientId}
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
       cacheLocation="localstorage"
       authorizationParams={{
         // eslint-disable-next-line camelcase
