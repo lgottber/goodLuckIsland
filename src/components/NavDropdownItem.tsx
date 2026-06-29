95fa347da6f3e183f8c0693373da091535af2bf1
rtrigoso <renzotrigoso@gmail.com>
2026-06-29 16:13:20 -0400

    reduce full page reload using client side routing

--- src/components/NavDropdownItem.tsx
+++ src/components/NavDropdownItem.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function NavDropdownItem({ href, label, onClose }: { href: string; label: string; onClose: () => void }) {
   return (
-    <a href={href} className="nav-dropdown-item" onClick={onClose}>
+    <Link href={href} className="nav-dropdown-item" onClick={onClose}>
       <span className="nav-dropdown-icon"></span> {label}
-    </a>
+    </Link>
   );
 }
