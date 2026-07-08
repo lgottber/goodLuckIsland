6db5b3eb83aa9d0bc0b3a8740aed2d6878d3df60
rtrigoso <renzotrigoso@gmail.com>
2026-07-08 18:30:03 -0400

    fixes rate limiting missing on pages

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -1,6 +1,5 @@
 interface CloudflareEnv extends Record<string, unknown> {
   AnalyticsEngineDataset: import("@cloudflare/workers-types").AnalyticsEngineDataset;
   DB: import("@cloudflare/workers-types").D1Database;
-  API_RATE_LIMITER: import("@cloudflare/workers-types").RateLimit;
-  ANON_RATE_LIMITER: import("@cloudflare/workers-types").RateLimit;
+  RATE_LIMIT_KV: import("@cloudflare/workers-types").KVNamespace;
 }
